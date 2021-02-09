5c60acc1c4bde8d843e541b0bd7d472e99ba2271
Dmytro Kulchytskyi <prodigy3452@gmail.com>
2021-02-09 06:07:23 +0200

    feat(base-repo): included the base repository in chargejs

--- src/infra/redis/index.js
+++ src/infra/redis/index.js
@@ -1,3 +1,3 @@
 export { default as redis } from './redis'
 export { default as RedisRepository } from './RedisRepository'
-export { default as redisStorage } from './RedisStorage'
+export { default as RedisStorage } from './RedisStorage'
