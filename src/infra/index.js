da43fecbaadac4b52cdedef2b1187e9d068ce99b
Dmytro Kulchytskyi <prodigy3452@gmail.com>
2021-10-29 06:56:42 +0300

    feat(cache-improve): improved caching

--- src/infra/index.js
+++ src/infra/index.js
@@ -1,5 +1,32 @@
+import { asClass, asFunction, asValue } from 'awilix'
+import { calculateLimitAndOffset, paginate } from 'paginate-info'
+import { Transaction, TransactionProvider } from './transaction'
+import { RedisRepository, RedisStorage, redis } from './redis'
+
+export { Transaction, TransactionProvider }
+export { RedisRepository, RedisStorage, redis }
+export { default as BaseRepository } from './BaseRepository'
 export { default as LoggerStreamAdapter } from './LoggerStreamAdapter'
 export { default as ModelLoader } from './ModelLoader'
-export { default as BaseRepository } from './BaseRepository'
-export * from './transaction'
-export * from './redis'
+
+export function register (container, repositories) {
+  container.register({
+    calculateLimitAndOffset: asValue(calculateLimitAndOffset),
+    paginate: asValue(paginate),
+    redis: asFunction(redis).singleton(),
+    redisStorage: asClass(RedisStorage).singleton(),
+    redisRepository: asClass(RedisRepository),
+    Transaction: asValue(Transaction),
+    transactionProvider: asClass(TransactionProvider).scoped()
+  })
+  container.register(Object.fromEntries(
+    Object.entries(repositories).map(([name, repository]) => [
+      _lovercaseName(name),
+      asClass(repository).scoped()
+    ])
+  ))
+}
+
+function _lovercaseName (name) {
+  return name.charAt(0).toLowerCase() + name.slice(1)
+}
